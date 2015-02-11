b2f57dac7323feb91605ddbd3c68f83612ada09b
Ben Ridge <ben@wanderaround.com>
2015-02-11 00:31:58 -0700

    public stuff moved to public dir. Working with node/express now

---
+++ server/node-server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.listen(80, function() {
+  console.log("Server listening on port 80");
+});
