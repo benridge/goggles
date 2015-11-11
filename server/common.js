1a402df796d9d0497d3fc82108db1b6c9d026859
Ben Ridge <ben@wanderaround.com>
2015-11-10 21:57:10 -0700

    fixed local environment config

--- server/common.js
+++ server/common.js
@@ -1,7 +1,7 @@
 var env = require("./env.json");
 
 var common = function() {
-  var node_env = process.env.NODE_ENV || 'development';
+  var node_env = process.env.NODE_ENV || 'local';
   return env[node_env];
 };
 
